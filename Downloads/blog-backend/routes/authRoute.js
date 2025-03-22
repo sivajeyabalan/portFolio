4c07b5fe7133b3ad287a2550c4d9b696c67e0f18
sivajeyabalan <sivajeyabalan15@gmail.com>
2025-03-22 15:16:56 +0530

    added few feature

--- Downloads/blog-backend/routes/authRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { login , register , logout } = require('../controllers/authController');  // Import login and register from authController , 
-// Example route
-router.post('/login', login);
-router.post('/register', register);
-router.get('/logout', logout);
-module.exports = router;  // Ensure this is exporting the router
